a1df67115835de24a4cee3c1d5f0572e982e9ff3
huf5 <152522942+huf5@users.noreply.github.com>
2024-03-28 18:11:05 -0400

    insert.updates

--- insert_data.sql
+++ insert_data.sql
@@ -1,29 +1,30 @@
 USE group_camp_query;
 
 INSERT INTO staff (staff_id, last_name, first_name, position_name, staff_phone_number, staff_email, staff_emergency_contact, staff_allergies, staff_dietary_restrictions, cpr_certification)
-	VALUES (14573, 'Adams', 'James', 'Counselor', '(254) 644-6473' , 'jaddams5@gmail.com', 'Jeremy Adams 2546446179', NULL, NULL, 'Y');
+	VALUES (17263, 'Adams', 'James', 'Counselor', '(254) 644-6473' , 'jaddams5@gmail.com', 'Jeremy Adams (254) 644-6179', NULL, NULL, 'Y');
 INSERT INTO staff (staff_id, last_name, first_name, position_name, staff_phone_number, staff_email, staff_emergency_contact, staff_allergies, staff_dietary_restrictions, cpr_certification)
-	VALUES (14577, 'Allen', 'Jaci', 'Counselor', '(771 653-2167' , 'kittyluver92.com', NULL, 'Peanuts', 'Vegetarian', 'Y');
+	VALUES (14577, 'Allen', 'Jaci', 'Counselor', '(771) 653-2167' , 'kittyluver92.com', NULL, 'Peanuts', 'Vegetarian', 'Y');
 INSERT INTO staff (staff_id, last_name, first_name, position_name, staff_phone_number, staff_email, staff_emergency_contact, staff_allergies, staff_dietary_restrictions, cpr_certification)
-	VALUES (13562, 'Jones', 'Helen', 'Activity Coordinator', '(254) 644-6473' , 'jaddams5@gmail.com', 'Jeremy Adams 2546446179', NULL, NULL, 'Y');
+	VALUES (13562, 'Jones', 'Helen', 'Activity Coordinator', '(536) 453-6473' , 'jones.helen@gmail.com', 'Jaci Jones (536) 453-6479', NULL, NULL, 'N');
 INSERT INTO staff (staff_id, last_name, first_name, position_name, staff_phone_number, staff_email, staff_emergency_contact, staff_allergies, staff_dietary_restrictions, cpr_certification)
-	VALUES (13338, 'Gilly', 'Cynthia', 'Activity Coordinator', '(254) 644-6473' , 'jaddams5@gmail.com', 'Jeremy Adams 2546446179', NULL, NULL, 'Y');
+	VALUES (13338, 'Gilly', 'Cynthia', 'Activity Coordinator', '(536) 222-6373' , 'cynthia.gilly@gmail.com', NULL , NULL, NULL, 'Y');
 INSERT INTO staff (staff_id, last_name, first_name, position_name, staff_phone_number, staff_email, staff_emergency_contact, staff_allergies, staff_dietary_restrictions, cpr_certification)
-	VALUES (12567, 'Flores', 'Zuri', 'Counselor', '(254) 644-6473' , 'jaddams5@gmail.com', 'Jeremy Adams 2546446179', NULL, NULL, 'Y');
+	VALUES (12567, 'Flores', 'Zuri', 'Junior Counselor', '(212) 254-6473' , 'zz42@gmail.com', NULL, NULL, NULL, 'N');
 INSERT INTO staff (staff_id, last_name, first_name, position_name, staff_phone_number, staff_email, staff_emergency_contact, staff_allergies, staff_dietary_restrictions, cpr_certification)
-	VALUES (14777, 'Xu', 'Marisol', 'Counselor', '(254) 644-6473' , 'jaddams5@gmail.com', 'Jeremy Adams 2546446179', NULL, NULL, 'Y');
+	VALUES (14777, 'Xu', 'Marisol', 'Counselor', '(223) 256-7584' , 'kittykat66@gmail.com', 'Jeremy Fennel (912) 647-7334', NULL, NULL, 'Y');
 INSERT INTO staff (staff_id, last_name, first_name, position_name, staff_phone_number, staff_email, staff_emergency_contact, staff_allergies, staff_dietary_restrictions, cpr_certification)
-	VALUES (13364, 'Brown', 'Jacob', 'Counselor', '(254) 644-6473' , 'jaddams5@gmail.com', 'Jeremy Adams 2546446179', NULL, NULL, 'Y');
+	VALUES (13364, 'Brown', 'Jacob', 'Counselor', '(254) 644-9264' , 'jjb5@gmail.com', 'Allen Coyne (254) 622-6273', NULL, NULL, 'Y');
 INSERT INTO staff (staff_id, last_name, first_name, position_name, staff_phone_number, staff_email, staff_emergency_contact, staff_allergies, staff_dietary_restrictions, cpr_certification)
-	VALUES (90145, 'Patel', 'Alley', 'Counselor', '(254) 644-6473' , 'jaddams5@gmail.com', 'Jeremy Adams 2546446179', NULL, NULL, 'Y');
+	VALUES (90145, 'Patel', 'Alley', 'Counselor', '(814) 224-0293' , 'alleycat@gmail.com', 'Riya Patel (335) 645-7568', NULL, NULL, 'N');
 INSERT INTO staff (staff_id, last_name, first_name, position_name, staff_phone_number, staff_email, staff_emergency_contact, staff_allergies, staff_dietary_restrictions, cpr_certification)
-	VALUES (90146, 'Renick', 'Dylan', 'Counselor', '(254) 644-6473' , 'jaddams5@gmail.com', 'Jeremy Adams 2546446179', NULL, NULL, 'Y');
+	VALUES (90146, 'Renick', 'Dylan', 'Junior Counselor', '(335) 222-0917' , 'doggylover@gmail.com', 'Terry Rennick (335) 657-6475' , NULL, NULL, 'Y');
 INSERT INTO staff (staff_id, last_name, first_name, position_name, staff_phone_number, staff_email, staff_emergency_contact, staff_allergies, staff_dietary_restrictions, cpr_certification)
-	VALUES (33456, 'Lennox', 'Owulatofu', 'Counselor', '(254) 644-6473' , 'jaddams5@gmail.com', 'Jeremy Adams 2546446179', NULL, NULL, 'Y');
+	VALUES (33456, 'Lennox', 'Owulatofu', 'Junior Counselor', '(254) 546-0192' , 'lennofu@gmail.com', 'Tanya Lennox (254) 546-7485', NULL, NULL, 'N');
 INSERT INTO staff (staff_id, last_name, first_name, position_name, staff_phone_number, staff_email, staff_emergency_contact, staff_allergies, staff_dietary_restrictions, cpr_certification)
-	VALUES (11145, 'Addams', 'Tiara', 'Counselor', '(254) 644-6473' , 'jaddams5@gmail.com', 'Jeremy Adams 2546446179', NULL, NULL, 'Y');
+	VALUES (11145, 'Addams', 'Tiara', 'Lead Chef', '(212) 543-9685' , 'tiaraaddams66@gmail.com', 'Diamond Addams (212) 647-6475', NULL, NULL, 'N');
 INSERT INTO staff (staff_id, last_name, first_name, position_name, staff_phone_number, staff_email, staff_emergency_contact, staff_allergies, staff_dietary_restrictions, cpr_certification)
-	VALUES (11535, 'Gacy', 'Diya', 'Counselor', '(254) 644-6473' , 'jaddams5@gmail.com', 'Jeremy Adams 2546446179', NULL, NULL, 'Y');
+	VALUES (11535, 'Gacy', 'Diya', 'Lifeguard', '(212) 536-7568' , 'dg664@gmail.com', 'Nona Gacy (212) 533-7529', NULL, NULL, 'Y');
     
 SELECT * FROM staff;
+
 
