d4630bd697eea934734f2c76bc20455b72d2fb0d
huf5 <152522942+huf5@users.noreply.github.com>
2024-04-10 20:19:34 -0400

    foreign key updates and data inserts

--- creation.sql
+++ creation.sql
@@ -1,25 +1,36 @@
 USE group_camp_query;
 
 SET FOREIGN_KEY_CHECKS=0;
--- Create staff table
-DROP TABLE IF EXISTS staff;
-CREATE TABLE staff(
-	staff_id INT PRIMARY KEY NOT NULL,
-    last_name VARCHAR(50) NOT NULL, 
-    first_name VARCHAR(50) NOT NULL,
-    position_name VARCHAR(100) NOT NULL,
-    staff_phone_number VARCHAR(50), 
-    staff_email VARCHAR(100) NOT NULL,
-    staff_emergency_contact VARCHAR(500),
-    staff_allergies VARCHAR(100),
-    staff_dietary_restrictions VARCHAR(100),
-    cpr_certification VARCHAR(1)
-)ENGINE INNODB;
+
+-- Create activities table
+DROP TABLE IF EXISTS activities;
+CREATE TABLE activities (
+    activity_id INT AUTO_INCREMENT PRIMARY KEY,
+    activity_name VARCHAR(100) NOT NULL,
+    age_group VARCHAR(50) NOT NULL,
+    activity_desc TEXT,
+    start_time TIME,
+    end_time TIME,
+    location VARCHAR(100),
+	-- instructor INT,
+    capacity INT,
+    activity_equipment INT,
+    activity_session INT
+) ENGINE INNODB;
+
+-- Create cabins table
+DROP TABLE IF EXISTS cabins;
+CREATE TABLE cabins (
+    cabin_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    cabin_name VARCHAR(50) NOT NULL,
+    capacity INT NOT NULL,
+    cab_group_id INT
+) ENGINE INNODB;
 
 -- Create campers table
 DROP TABLE IF EXISTS campers;
-CREATE TABLE campers(
-	camper_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+CREATE TABLE campers (
+    camper_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     gender VARCHAR(1) NOT NULL,
@@ -33,100 +44,27 @@ CREATE TABLE campers(
     group_assignment INT NOT NULL
 ) ENGINE INNODB;
 
--- Create transactions table
-DROP TABLE IF EXISTS transactions;
-CREATE TABLE transactions(
-	transaction_id INT PRIMARY KEY,
-    transaction_type VARCHAR(20),
-    amount INT,
-    transaction_date DATETIME,
-	payment_type VARCHAR(20), 
-    trans_camper INT NOT NULL, 
-    trans_session INT NOT NULL
-)ENGINE INNODB;
+-- Create camper_groups table
+DROP TABLE IF EXISTS camper_groups;
+CREATE TABLE camper_groups (
+    group_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    group_name VARCHAR(50), 
+    cg_cabin INT NOT NULL, 
+    cg_staff INT NOT NULL
+) ENGINE INNODB;
 
--- Create guradians table
+-- Create guardians table
 DROP TABLE IF EXISTS guardians;
-CREATE TABLE guardians(
-	guardian_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+CREATE TABLE guardians (
+    guardian_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
+    camper INT, 
     relationship_to_camper VARCHAR(50) NOT NULL,
     home_address VARCHAR(100) NOT NULL,
     phone_number VARCHAR(15) NOT NULL,
     email VARCHAR(50) NOT NULL
-)ENGINE INNODB;
-
--- Create cabins table
-DROP TABLE IF EXISTS cabins;
-CREATE TABLE cabins(
-	cabin_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    cabin_name VARCHAR(50) NOT NULL,
-    capacity INT NOT NULL,
-    cab_group_id INT
-)ENGINE INNODB;
-
--- Create camper_groups
-DROP TABLE IF EXISTS camper_groups;
-CREATE TABLE camper_groups(
-	group_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    group_name VARCHAR(50), 
-    cg_cabin INT NOT NULL, 
-    cg_staff INT NOT NULL
-)ENGINE INNODB;
-
--- create sessions table
-DROP TABLE IF EXISTS sessions;
-CREATE TABLE sessions(
-	session_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    theme VARCHAR(50) NOT NULL,
-    enrollment_capacity INT NOT NULL,
-    registration_deadline DATE NOT NULL,
-    session_status VARCHAR(50) NOT NULL,
-    session_fee INT NOT NULL, 
-    s_group INT
-)ENGINE INNODB;
-
--- create transportation table
-DROP TABLE IF EXISTS transportation;
-CREATE TABLE transportation(
-	vehicle_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    route VARCHAR(50), 
-    vehicle_type VARCHAR(50) NOT NULL,
-    capacity INT NOT NULL,
-    wheelchair_accesssible VARCHAR(1) NOT NULL,
-    driver INT NOT NULL
-)ENGINE INNODB;
-
--- Create supplies table and add fk and drop it
-DROP TABLE IF EXISTS supplies; 
-CREATE TABLE supplies(
-	item_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    item_name VARCHAR(100) NOT NULL, 
-    quantity INT NOT NULL, 
-    supplier VARCHAR(100) NOT NULL, 
-    cost INT NOT NULL, 
-    order_date DATETIME NOT NULL, 
-    delivery_date DATETIME NOT NULL
-)ENGINE INNODB;
-
--- Create activities table
-DROP TABLE IF EXISTS activites;
-CREATE TABLE activities (
-    activity_id INT AUTO_INCREMENT PRIMARY KEY,
-    activity_name VARCHAR(100) NOT NULL,
-    age_group VARCHAR(50) NOT NULL,
-    activity_desc TEXT,
-    start_time DATETIME,
-    end_time DATETIME,
-    location VARCHAR(100),
-    instructor VARCHAR(100),
-    capacity INT,
-    CONSTRAINT chk_capacity CHECK (capacity > 0)
 ) ENGINE INNODB;
-
 
 -- Create health records table 
 DROP TABLE IF EXISTS health_records;
@@ -145,64 +83,181 @@ CREATE TABLE health_records (
     doctor_contact_number VARCHAR(20)
 ) ENGINE INNODB;
 
+-- Create sessions table
+DROP TABLE IF EXISTS sessions;
+CREATE TABLE sessions (
+    session_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    theme VARCHAR(50) NOT NULL,
+    enrollment_capacity INT NOT NULL,
+    registration_deadline DATE NOT NULL,
+    session_status VARCHAR(50) NOT NULL,
+    session_fee INT NOT NULL, 
+    s_group INT
+) ENGINE INNODB;
+
+-- Create staff table
+DROP TABLE IF EXISTS staff;
+CREATE TABLE staff (
+    staff_id INT PRIMARY KEY NOT NULL,
+    last_name VARCHAR(50) NOT NULL, 
+    first_name VARCHAR(50) NOT NULL,
+    position_name VARCHAR(100) NOT NULL,
+    staff_phone_number VARCHAR(50), 
+    staff_email VARCHAR(100) NOT NULL,
+    staff_emergency_contact VARCHAR(500),
+    staff_allergies VARCHAR(100),
+    staff_dietary_restrictions VARCHAR(100),
+    cpr_certification VARCHAR(1)
+) ENGINE INNODB;
+
+-- Create supplies table
+DROP TABLE IF EXISTS supplies; 
+CREATE TABLE supplies (
+    item_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    item_name VARCHAR(100) NOT NULL, 
+    quantity INT NOT NULL, 
+    supplier VARCHAR(100) NOT NULL, 
+    cost INT NOT NULL, 
+    order_date DATETIME NOT NULL, 
+    delivery_date DATETIME NOT NULL, 
+    activity_id INT
+) ENGINE INNODB;
+
+-- Create transactions table
+DROP TABLE IF EXISTS transactions;
+CREATE TABLE transactions (
+    transaction_id INT PRIMARY KEY,
+    transaction_type VARCHAR(20),
+    amount INT,
+    transaction_date DATETIME,
+    payment_type VARCHAR(20), 
+    trans_camper INT NOT NULL, 
+    trans_session INT NOT NULL
+) ENGINE INNODB;
+
+-- Create transportation table
+DROP TABLE IF EXISTS transportation;
+CREATE TABLE transportation (
+    vehicle_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    route VARCHAR(50), 
+    vehicle_type VARCHAR(50) NOT NULL,
+    capacity INT NOT NULL,
+    wheelchair_accessible VARCHAR(10) NOT NULL,
+    driver INT NOT NULL
+) ENGINE INNODB;
 
 SET FOREIGN_KEY_CHECKS=1;
--- ALTER campers table to add guardian fk and drop if exists
+
+-- Alter campers table to add guardian fk
 ALTER TABLE campers
-	ADD CONSTRAINT fk_guardian
+    ADD CONSTRAINT fk_guardian
     FOREIGN KEY (guardian)
     REFERENCES guardians (guardian_id);
+
+-- Alter campers table to add group_assignment fk
 ALTER TABLE campers
-	ADD CONSTRAINT fk_group_assignment
+    ADD CONSTRAINT fk_group_assignment
     FOREIGN KEY (group_assignment)
     REFERENCES camper_groups (group_id);
-ALTER TABLE campers DROP CONSTRAINT fk_guardian;
-ALTER TABLE campers DROP CONSTRAINT fk_group_assignment;
 
--- Alter transactions to includes foreign keys and drop if created
+-- Alter transactions to add fk
 ALTER TABLE transactions 
-	ADD CONSTRAINT fk_trans_camper
+    ADD CONSTRAINT fk_trans_camper
     FOREIGN KEY (trans_camper)
     REFERENCES campers (camper_id);
+
 ALTER TABLE transactions
-	ADD CONSTRAINT fk_trans_session
+    ADD CONSTRAINT fk_trans_session
     FOREIGN KEY (trans_session)
     REFERENCES sessions (session_id);
-ALTER TABLE transactions DROP CONSTRAINT fk_trans_camper;
-ALTER TABLE transactions DROP CONSTRAINT fk_trans_session;
 
--- ALTER cabins table to add fk and drop if made already. 
+-- Alter cabins table to add fk
 ALTER TABLE cabins 
-	ADD CONSTRAINT fk_c_groups
+    ADD CONSTRAINT fk_c_groups
     FOREIGN KEY (cab_group_id)
     REFERENCES camper_groups(group_id); 
-ALTER TABLE cabins DROP CONSTRAINT fk_c_groups;
 
--- Alter camper_groups
+-- Alter camper_groups to add fk
 ALTER TABLE camper_groups 
-	ADD CONSTRAINT fk_cg_cabin
+    ADD CONSTRAINT fk_cg_cabin
     FOREIGN KEY (cg_cabin)
     REFERENCES cabins (cabin_id);
+
 ALTER TABLE camper_groups 
-	ADD CONSTRAINT fk_cg_staff
+    ADD CONSTRAINT fk_cg_staff
     FOREIGN KEY (cg_staff)
     REFERENCES staff (staff_id);
-ALTER TABLE camper_groups DROP CONSTRAINT fk_cg_cabin;
-ALTER TABLE camper_groups DROP CONSTRAINT fk_cg_staff;
 
--- Alter sessions
+-- Alter sessions to add fk
 ALTER TABLE sessions
-	ADD CONSTRAINT fk_session_gid
+    ADD CONSTRAINT fk_session_gid
     FOREIGN KEY (s_group)
     REFERENCES camper_groups (group_id);
-ALTER TABLE sessions DROP CONSTRAINT fk_session_gid;
 
--- ALTER transportation
+-- Alter transportation to add fk
 ALTER TABLE transportation
-	ADD CONSTRAINT fk_driver
-	FOREIGN KEY (driver)
-	REFERENCES staff(staff_id);
-ALTER TABLE transportation DROP CONSTRAINT fk_driver;
+    ADD CONSTRAINT fk_driver
+    FOREIGN KEY (driver)
+    REFERENCES staff(staff_id);
+
+-- Alter activities to add fk
+ALTER TABLE activities
+    ADD CONSTRAINT fk_activity_equipment
+    FOREIGN KEY (activity_equipment)
+    REFERENCES supplies(item_id);
+
+ALTER TABLE activities
+    ADD CONSTRAINT fk_activity_session
+    FOREIGN KEY (activity_session)
+    REFERENCES sessions(session_id);
+
+    
+/* ALTER TABLE activities
+    ADD CONSTRAINT fk_instructor
+    FOREIGN KEY (activity_instructor)
+    REFERENCES staff(staff_id); */ 
+    
+-- Alter health records table
+ALTER TABLE health_records
+	ADD CONSTRAINT fk_hr_camper_id
+    FOREIGN KEY (camper_id)
+    REFERENCES campers(camper_id);
+
+-- Alter table supplies
+ALTER TABLE supplies
+	ADD CONSTRAINT fk_actvity
+    FOREIGN KEY (activity_id)
+    REFERENCES activities(activity_id);
+    
+-- Alter guardians table
+ALTER TABLE guardians
+	ADD CONSTRAINT fk_camper
+    FOREIGN KEY (camper)
+    REFERENCES campers(camper_id);
+
+#Insert data into the activities table
+INSERT INTO activities (activity_name, age_group, activity_desc, start_time, end_time, location, /* activity_instructor */ capacity, activity_equipment, activity_session)
+	VALUES ("Nature Hike", "7-18", "Guided hike through nearby trails to explore local flora and fauna.", "09:00:00", "12:00:00", "Camp Query Forest", 20, NULL, NULL);
+INSERT INTO activities (activity_name, age_group, activity_desc, start_time, end_time, location, /* activity_instructor */ capacity, activity_equipment, activity_session)
+	VALUES ("Campfire Smores", "5-18", "Gather around a campfire for storytelling and marshmallow roasting.", "8:00:00", "10:00:00", "Camp Query Town Hall", 200, NULL, NULL);
+INSERT INTO activities (activity_name, age_group, activity_desc, start_time, end_time, location, /* activity_instructor */ capacity, activity_equipment, activity_session)
+	VALUES ("Arts and Crafts", "7-18", "Create personalized souvenirs using materials found in nature.", "09:00:00", "12:00:00", "Arts Center", 35, NULL, NULL);
+INSERT INTO activities (activity_name, age_group, activity_desc, start_time, end_time, location, /* activity_instructor */ capacity, activity_equipment, activity_session)
+	VALUES ("Baseball", "7-18", "Play classic baseball team vs team", "02:00:00", "04:00:00", "Baseball Diamond", 22, NULL, NULL);
+INSERT INTO activities (activity_name, age_group, activity_desc, start_time, end_time, location, /* activity_instructor */ capacity, activity_equipment, activity_session)
+	VALUES ("Volleyball", "7-18", "Play classic volleyball team vs team", "05:00:00", "07:00:00", "Bigelow Beach", 100, NULL, NULL);
+INSERT INTO activities (activity_name, age_group, activity_desc, start_time, end_time, location, /* activity_instructor */ capacity, activity_equipment, activity_session)
+	VALUES ("Swimming and Water Sports", "5-18", "Enjoy swimming, water volleyball, or pillow jumping.", "12:00:00", "08:00:00", "Bigelow Lake", 20, NULL, NULL);
+INSERT INTO activities (activity_name, age_group, activity_desc, start_time, end_time, location, /* activity_instructor */ capacity, activity_equipment, activity_session)
+	VALUES ("Photogrpahy Expidition", "5-18", "Capture the beauty of nature through photography sessions led by a professional.", "12:00:00", "03:00:00", "Camp Query Forest", 20, NULL, NULL);
+INSERT INTO activities (activity_name, age_group, activity_desc, start_time, end_time, location, /* activity_instructor */ capacity, activity_equipment, activity_session)
+	VALUES ("Capture the Flag", "5-18", "Join team to fight for the flag.", "05:00:00", "07:00:00", "Camp Query Forest", 20, NULL, NULL);
+INSERT INTO activities (activity_name, age_group, activity_desc, start_time, end_time, location, /* activity_instructor */ capacity, activity_equipment, activity_session)
+	VALUES ("Conoeing and Kayaking", "10-18", "Learn basic paddling techniques and navigate through calm waters.", "11:00:00", "06:00:00", "Bigelow Lake", 35, NULL, NULL);
+INSERT INTO activities (activity_name, age_group, activity_desc, start_time, end_time, location, /* activity_instructor */ capacity, activity_equipment, activity_session)
+	VALUES ("Ropes Course", "7-18", "Use teamwork and survival skills to navigate a 100 foot high rope maze", "09:00:00", "12:00:00", "Camp Query Forest", 15, NULL, NULL);
 
 #Insert data into staff table
 INSERT INTO staff (staff_id, last_name, first_name, position_name, staff_phone_number, staff_email, staff_emergency_contact, staff_allergies, staff_dietary_restrictions, cpr_certification)
@@ -280,12 +335,7 @@ INSERT INTO camper_groups (group_name, cg_cabin, cg_staff)
 	VALUES ('Purple', 9, 90146);
 INSERT INTO camper_groups (group_name, cg_cabin, cg_staff)
 	VALUES ('Brown', 10, 33456);
-INSERT INTO camper_groups (group_name, cg_cabin, cg_staff)
-	VALUES ('Rainbow', 11, 11145);
-INSERT INTO camper_groups (group_name, cg_cabin, cg_staff)
-	VALUES ('Orange', 12, 11535);
-INSERT INTO camper_groups (group_name, cg_cabin, cg_staff)
-	VALUES ('Indigo', 13, 11256);
+
     
 #Insert data into into the sessions table
 INSERT INTO sessions (start_date, end_date, theme, enrollment_capacity, registration_deadline, session_status, session_fee)
@@ -312,25 +362,25 @@ INSERT INTO sessions (start_date, end_date, theme, enrollment_capacity, registra
 	VALUES ('2024-12-27', '2025-01-07', 'Fine Arts', 50, '2024-10-15', 'REGISTRATION SOON', 575.00);
     
 #Insert data into transportation table
-INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accesssible, driver)
+INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accessible, driver)
 	VALUES ("Pine Grove", "SUV", 6, "N", 14777);
-INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accesssible, driver)
+INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accessible, driver)
 	VALUES ("Pine Grove", "Bus", 30, "Y", 12567);
-INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accesssible, driver)
+INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accessible, driver)
 	VALUES ("Sleepy Rocks", "SUV", 6, "N", 13562);
-INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accesssible, driver)
+INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accessible, driver)
 	VALUES ("Sleepy Rocks", "SUV", 6, "N", 14777);
-INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accesssible, driver)
+INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accessible, driver)
 	VALUES ("Portland Center", "Bus", 30, "Y", 33456);
-INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accesssible, driver)
+INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accessible, driver)
 	VALUES ("Ruffaloville", "SUV", 6, "N", 11145);
-INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accesssible, driver)
+INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accessible, driver)
 	VALUES ("Sailing", "Truck", 6, "N", 17263);
-INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accesssible, driver)
+INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accessible, driver)
 	VALUES ("Sailing", "SUV", 6, "N", 13338);
-INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accesssible, driver)
+INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accessible, driver)
 	VALUES ("Sailing", "Truck", 6, "N", 11535);
-INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accesssible, driver)
+INSERT INTO transportation (route, vehicle_type, capacity, wheelchair_accessible, driver)
 	VALUES ("Piney Forest", "SUV", 6, "N", 13338);
 
 -- Insert infromation into the supplies table
@@ -356,19 +406,17 @@ INSERT INTO supplies (item_name, quantity, supplier, cost, order_date, delivery_
 	VALUES("Baseball Kit", 20, "Dick's Sporting Goods", 35.89, "2022-04-13", "2011-08-09");
 INSERT INTO supplies (item_name, quantity, supplier, cost, order_date, delivery_date)
 	VALUES("Storage Shed", 1, "Home Depot", 5175.99, "2010-09-22", "2010-10-03");
-    
+
 
 -- Show the tables
+SELECT * FROM activities;
+SELECT * FROM cabins;
+SELECT * FROM campers;
+SELECT * FROM camper_groups;
+SELECT * FROM guardians;
+SELECT * FROM health_records;
+SELECT * FROM sessions;
+SELECT * FROM supplies;
 SELECT * FROM staff;
 SELECT * FROM transactions;
-
-SELECT * FROM campers;
-SELECT * FROM guardians;
-
-SELECT * FROM cabins;
-SELECT * FROM camper_groups;
-SELECT * FROM sessions;
-
 SELECT * FROM transportation;
-
-SELECT * FROM supplies;
